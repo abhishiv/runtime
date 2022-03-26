e7c901426aa02277f99c9a61b5ea1e7d0ea1920d
Abhishiv Saxena <abhishiv@gmail.com>
2022-03-26 05:24:39 +0200

    less logs

--- src/pm/utils/file_system.ts
+++ src/pm/utils/file_system.ts
@@ -1,32 +1,44 @@
-import { IPackageManagerProps, ILogicalTree, PkgData, PkgTreeNode, PkgDirectory, PkgFile } from '../../specs'
-import crawl from 'tree-crawl'
-import groupBy from 'lodash.groupby'
-import { IFileSystem as IFilesystem, path as nodePath, mkdirP, normalizePath } from '@gratico/fs'
-import promisify from 'pify'
-import { logicalTreeAdressToFSPath } from './dependency_tree'
-import path, { basename } from 'path'
+import {
+  IPackageManagerProps,
+  ILogicalTree,
+  PkgData,
+  PkgTreeNode,
+  PkgDirectory,
+  PkgFile,
+} from "../../specs";
+import crawl from "tree-crawl";
+import groupBy from "lodash.groupby";
+import {
+  IFileSystem as IFilesystem,
+  path as nodePath,
+  mkdirP,
+  normalizePath,
+} from "@gratico/fs";
+import promisify from "pify";
+import { logicalTreeAdressToFSPath } from "./dependency_tree";
+import path, { basename } from "path";
 
 export interface IntermediateFileTree {
-  tree: ILogicalTree
-  path: string
-  pkgData: PkgData
+  tree: ILogicalTree;
+  path: string;
+  pkgData: PkgData;
 }
 
 export type FileList =
   | { pathList: string[]; fileName: string; isFolder: true }
-  | { pathList: string[]; isFolder: false; fileName: string; content: string }
+  | { pathList: string[]; isFolder: false; fileName: string; content: string };
 export async function flushFileTree(
   props: IPackageManagerProps,
   logicalTree: ILogicalTree,
   downloadedPackages: {
-    pkgData: PkgData
-    trees: { id: string; tree: ILogicalTree }[]
-  }[],
+    pkgData: PkgData;
+    trees: { id: string; tree: ILogicalTree }[];
+  }[]
 ) {
-  const list: IntermediateFileTree[] = []
+  const list: IntermediateFileTree[] = [];
 
   for (var key in Object.keys(downloadedPackages)) {
-    const item = downloadedPackages[key]
+    const item = downloadedPackages[key];
     list.splice(
       list.length,
       0,
@@ -34,71 +46,80 @@ export async function flushFileTree(
         tree: tree.tree,
         path: logicalTreeAdressToFSPath(tree.tree.address),
         pkgData: item.pkgData,
-      })),
-    )
+      }))
+    );
   }
 
-  const jobs: [string, FileList[]][] = []
+  const jobs: [string, FileList[]][] = [];
   list.forEach((item) => {
-    const { pkgData, path, tree } = item
+    const { pkgData, path, tree } = item;
 
     crawl<PkgTreeNode>(
       pkgData.fileTree,
       (node) => {
-        const filePath = nodePath.join(props.workingDirectory, 'node_modules', path, node.fullName)
-        const pathList = filePath.split('/')
-        const baseName = nodePath.dirname(filePath)
+        const filePath = nodePath.join(
+          props.workingDirectory,
+          "node_modules",
+          path,
+          node.fullName
+        );
+        const pathList = filePath.split("/");
+        const baseName = nodePath.dirname(filePath);
         const j: [string, FileList[]] =
           jobs.find((el) => el[0] === baseName) ||
           (function () {
-            const entry: [string, FileList[]] = [baseName, []]
-            jobs.push(entry)
-            return entry
-          })()
-        if (node.type === 'directory')
+            const entry: [string, FileList[]] = [baseName, []];
+            jobs.push(entry);
+            return entry;
+          })();
+        if (node.type === "directory")
           j[1].push({
             pathList,
             fileName: node.name,
             isFolder: true,
-          })
-        if (node.type === 'file' && (pkgData.vendorFiles[node.fullName] || node.name === 'package.json'))
+          });
+        if (
+          node.type === "file" &&
+          (pkgData.vendorFiles[node.fullName] || node.name === "package.json")
+        )
           j[1].push({
             pathList,
             fileName: node.name,
             isFolder: false,
             content: pkgData.vendorFiles[node.fullName],
-          })
+          });
       },
-      { getChildren: (node) => (node.type === 'directory' ? node.files : []) },
-    )
-  })
+      { getChildren: (node) => (node.type === "directory" ? node.files : []) }
+    );
+  });
 
-  console.log('jobs', jobs[1])
+  console.info("jobs", jobs[1]);
 
-  const { workingDirectory: workDir, fs } = props
-  //  console.log(fs)
+  const { workingDirectory: workDir, fs } = props;
 
-  const tasks = []
+  const tasks = [];
   for (let entry of jobs) {
-    const [baseName, items] = entry
+    const [baseName, items] = entry;
     tasks.push(async () => {
       await Promise.all(
         items.map(async (item) => {
-          const path = nodePath.join(item.pathList.join('/'))
+          const path = nodePath.join(item.pathList.join("/"));
           if (item.isFolder) {
             try {
-              await mkdirP(fs, path)
+              await mkdirP(fs, path);
             } catch (e) {
-              console.error(item.fileName, item.pathList)
+              console.error(item.fileName, item.pathList);
             }
           } else {
-            await promisify(fs.writeFile)(path, item.content, { encoding: 'utf8' })
+            await promisify(fs.writeFile)(path, item.content, {
+              encoding: "utf8",
+            });
           }
-        }),
-      )
-    })
+        })
+      );
+    });
   }
   for await (const task of tasks) {
-    await task()
+    await task();
   }
 }
--- src/pm/utils/populate.ts
+++ src/pm/utils/populate.ts
@@ -1,54 +1,59 @@
-import { IFileSystem as IFilesystem, path as nodePath } from '@gratico/fs'
-import promisify from 'pify'
-import { mkdirP, normalizePath } from '@gratico/fs'
+import { IFileSystem as IFilesystem, path as nodePath } from "@gratico/fs";
+import promisify from "pify";
+import { mkdirP, normalizePath } from "@gratico/fs";
 
-import { fetchPkgData } from '../../npm'
-import { IPackageManagerProps, ILogicalTree } from '../../specs'
+import { fetchPkgData } from "../../npm";
+import { IPackageManagerProps, ILogicalTree } from "../../specs";
 
-import { getAddressList, logicalTreeAdressToFSPath, getLogicalTree } from './dependency_tree'
-import { flushFileTree, IntermediateFileTree } from './file_system'
+import {
+  getAddressList,
+  logicalTreeAdressToFSPath,
+  getLogicalTree,
+} from "./dependency_tree";
+import { flushFileTree, IntermediateFileTree } from "./file_system";
 
-export async function populateFileSystem(props: IPackageManagerProps, logicalTree: ILogicalTree) {
-  const { fs, workingDirectory: workDir } = props
-  console.log('workDir', workDir)
+export async function populateFileSystem(
+  props: IPackageManagerProps,
+  logicalTree: ILogicalTree
+) {
+  const { fs, workingDirectory: workDir } = props;
   try {
-    await promisify(fs.mkdir)(nodePath.join(workDir, 'node_modules'))
+    await promisify(fs.mkdir)(nodePath.join(workDir, "node_modules"));
   } catch (e) {}
   const packages = await computeMissingPackages({
     fs: props.fs,
     workDir: props.workingDirectory,
     logicalTree,
-  })
+  });
 
-  console.log('missingPackages', Object.keys(packages).length)
+  console.info("missingPackages", Object.keys(packages).length);
   const downloadPackages = await Promise.all(
     Object.values(packages).map(async (trees) => {
-      const tree = trees[0].tree
-      const pkgData = await fetchPkgData(tree.name, tree.version, props.fetch)
+      const tree = trees[0].tree;
+      const pkgData = await fetchPkgData(tree.name, tree.version, props.fetch);
       // be carefull to store pkgData on all neded locations
-      return { pkgData, trees }
-    }),
-  )
-  // storePackage(props, pkgData, treeObject.tree)
-  console.log('flusging file tree')
-  await flushFileTree(props, logicalTree, downloadPackages)
+      return { pkgData, trees };
+    })
+  );
+  console.info("flushFileTree");
+  await flushFileTree(props, logicalTree, downloadPackages);
 
-  const logicalTree2 = await getLogicalTree(props.fs, props.workingDirectory)
-  const pkgsDict = getAddressList(logicalTree2)
+  const logicalTree2 = await getLogicalTree(props.fs, props.workingDirectory);
+  const pkgsDict = getAddressList(logicalTree2);
   const tasks = Object.values(pkgsDict)
     .filter((el) => el.version)
     .map(async (pkg) => {
       const filePath = nodePath.join(
         props.workingDirectory,
-        'node_modules',
+        "node_modules",
         logicalTreeAdressToFSPath(pkg.address),
-        'package.json',
-      )
-      const manifestText = await promisify(fs.readFile)(filePath)
-      return JSON.parse(manifestText)
-    })
-  const manifests = await Promise.all(tasks)
-  return manifests
+        "package.json"
+      );
+      const manifestText = await promisify(fs.readFile)(filePath);
+      return JSON.parse(manifestText);
+    });
+  const manifests = await Promise.all(tasks);
+  return manifests;
 }
 
 // since one package can be on multiple locations in fs theoratically
@@ -57,37 +62,47 @@ export async function computeMissingPackages({
   fs,
   workDir,
 }: {
-  logicalTree: ILogicalTree
-  fs: IFilesystem
-  workDir: string
+  logicalTree: ILogicalTree;
+  fs: IFilesystem;
+  workDir: string;
 }) {
-  const addressList = getAddressList(logicalTree)
+  const addressList = getAddressList(logicalTree);
   const packagesList = Object.keys(addressList)
     .filter((el) => !addressList[el].isRoot)
     .map((address) => ({
-      id: nodePath.join(workDir, 'node_modules', logicalTreeAdressToFSPath(address)),
+      id: nodePath.join(
+        workDir,
+        "node_modules",
+        logicalTreeAdressToFSPath(address)
+      ),
       value: addressList[address],
-    }))
+    }));
   const packageFiles = (
     (await fs.adapter.query({
-      name: 'package.json',
+      name: "package.json",
     })) as { parentPath: string }[]
   ).filter((el) => {
-    return el.parentPath && el.parentPath.match(new RegExp(workDir))
-  })
+    return el.parentPath && el.parentPath.match(new RegExp(workDir));
+  });
   const missingPackages = packagesList.filter(({ value: pkg }) => {
-    const folderPath = normalizePath(nodePath.join(workDir, 'node_modules', logicalTreeAdressToFSPath(pkg.address)))
-    return packageFiles.findIndex((el) => el.parentPath === folderPath) === -1
-  })
-  const packages = [...new Set(missingPackages)]
+    const folderPath = normalizePath(
+      nodePath.join(
+        workDir,
+        "node_modules",
+        logicalTreeAdressToFSPath(pkg.address)
+      )
+    );
+    return packageFiles.findIndex((el) => el.parentPath === folderPath) === -1;
+  });
+  const packages = [...new Set(missingPackages)];
   return packages.reduce<{
-    [key: string]: { id: string; tree: ILogicalTree }[]
+    [key: string]: { id: string; tree: ILogicalTree }[];
   }>((state, { value, id }) => {
-    if (!value.version || !value.name) return state
-    const key = value.name + '@' + value.version
+    if (!value.version || !value.name) return state;
+    const key = value.name + "@" + value.version;
     return {
       ...state,
       [key]: [...(state[key] || []), { id, tree: value }],
-    }
-  }, {})
+    };
+  }, {});
 }
